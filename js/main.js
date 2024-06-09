fc14e41065ccff26640563b93c85054f2612a4e3
Johrespi <johrespi@espol.edu.ec>
2024-06-08 23:07:42 -0500

    form y js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,47 @@
-let loaded = (eventLoaded) => {
+const formulario = document.getElementById('formulario');
+const url = 'https://landing-d3a5e-default-rtdb.firebaseio.com/adopcion.json';
+
+formulario.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+    const cedula = document.getElementById('cedula').value;
+    const telefono = document.getElementById('telefono').value;
+    const direccion = document.getElementById('direccion').value;
+
+    const datos = {
+        nombre: nombre,
+        apellido: apellido,
+        email: email,
+        cedula: cedula,
+        telefono: telefono,
+        direccion: direccion,
+    };
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(datos),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    } )
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        console.log(datos);
+    })
+    .catch(error => console.error(error));
+});
+
+
+
+/*let loaded = (eventLoaded) => {
     let myform = document.getElementById('formulario');
     myform.addEventListener('submit', (eventSubmit) => eventSubmit{})
     debugger;
 }
 
-window.addEventListener("DOMContentLoaded", loaded);
+window.addEventListener("DOMContentLoaded", loaded);*/
 
