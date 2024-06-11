0187dcc17ff616eee24232909eea95a25e35cfa2
Johrespi <johrespi@espol.edu.ec>
2024-06-11 00:55:22 -0500

    get form

--- js/main.js
+++ js/main.js
@@ -1,50 +1,85 @@
 const formulario = document.getElementById('formulario');
 const url = 'https://landing-d3a5e-default-rtdb.firebaseio.com/adopcion.json';
 
-formulario.addEventListener('submit', (event) => {
-    
-    event.preventDefault();
+let loaded = (eventLoaded) => {
+    let myform = document.getElementById('formulario');
+    myform.addEventListener('submit', (eventSubmit) => {
+        eventSubmit.preventDefault(); 
 
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const email = document.getElementById('email').value;
-    const cedula = document.getElementById('cedula').value;
-    const telefono = document.getElementById('telefono').value;
-    const direccion = document.getElementById('direccion').value;
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const email = document.getElementById('email').value;
+        const cedula = document.getElementById('cedula').value;
+        const telefono = document.getElementById('telefono').value;
+        const direccion = document.getElementById('direccion').value;
+        const opcion = formulario.opcion.value;
 
-    const datos = {
-        nombre: nombre,
-        apellido: apellido,
-        email: email,
-        cedula: cedula,
-        telefono: telefono,
-        direccion: direccion,
-    };
-
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(datos),
-        headers:{
-            'Content-Type': 'application/json'
+        if (!nombre) {
+            alert("Por favor, ingresa tu nombre.");
+            return;
         }
-    } )
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        console.log(datos);
 
-    })
-    .catch(error => console.error(error));
-});
+        if (!apellido) {
+            alert("Por favor, ingresa tu apellido.");
+            return;
+        }
 
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+(\.[^\s@]+)*$/;
 
-function limpiarCampos() {
-    document.getElementById('nombre').value = '';
-    document.getElementById('apellido').value = '';
-    document.getElementById('email').value = ''; 
-    document.getElementById('cedula').value = ''; 
-    document.getElementById('telefono').value = ''; 
-    document.getElementById('direccion').value = ''; 
+        if (!emailRegex.test(email)) {
+            alert("Por favor, ingresa un correo electrónico válido.");
+            return;
+        }
+
+        if (cedula.length !== 10) {
+            alert("Por favor, ingresa una cédula válida de 10 dígitos.");
+            return;
+        }
+
+        if (telefono.length !== 9) {
+            alert("Por favor, ingresa un número de teléfono válido de 9 dígitos.");
+            return;
+        }
+
+        if (!direccion) {
+            alert("Por favor, ingresa tu dirección.");
+            return;
+        }
+
+        if (!opcion) {
+            alert("Por favor, selecciona una opción.");
+            return;
+        }
+
+        const datos = {
+            nombre: nombre,
+            apellido: apellido,
+            email: email,
+            cedula: cedula,
+            telefono: telefono,
+            direccion: direccion,
+            opcion: opcion
+        };
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            console.log(datos);
+            obtenerDatos();
+        })
+        .catch(error => console.error(error));
+    });
+
+    debugger;
 }
+
+window.addEventListener("DOMContentLoaded", loaded);
 
 async function obtenerDatos(){
 
@@ -59,6 +94,7 @@ async function obtenerDatos(){
     console.log(datos);
 
     let template = '';
+    let conteoTotal = 0;
 
     for (const key in datos) {
         if (Object.hasOwnProperty.call(datos, key)) {
@@ -67,16 +103,25 @@ async function obtenerDatos(){
                 <tr>
                     <td>${elemento.apellido}</td>
                     <td>${elemento.nombre}</td>
+                     <td>${elemento.cedula}</td>
                 </tr>
             `;
+            conteoTotal++;
         }
     }
 
-    const tabla = document.getElementById('tabla');
-    tabla.innerHTML = template;
-}
+    template += `
+    <tr>
+        <td colspan="2"><strong>Total de Aspirantes</strong></td>
+        <td><strong>${conteoTotal}</strong></td>
+    </tr>
+`;
 
-obtenerDatos();
+    const tabla = document.getElementById('tablebody');
+    tabla.innerHTML = template;
+} 
+
+obtenerDatos()
 
 /* hacerlo con el evento submit*/
 /*let loaded = (eventLoaded) => {
